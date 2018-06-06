3c5d45b786e2435ce233c630987e7f6e2fe4c0b6
Adrian Ramiro <33880273+AdrianRamiroEscamilla@users.noreply.github.com>
2018-06-06 17:39:02 +0200

    t

---
+++ Agenda/src/main/test/com/springmvc/test/TestPrueba1.java
@@ -0,0 +1,32 @@
+package com.springmvc.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.rules.SpringClassRule;
+import org.springframework.test.context.junit4.rules.SpringMethodRule;
+
+import com.springmvc.config.ApplicationContextConfig;
+import com.springmvc.dao.PersonasDAO;
+import com.springmvc.dao.PersonasDAOImpl;
+
+@ContextConfiguration(classes = {ApplicationContextConfig.class})
+public class TestPrueba1 
+{
+	@ClassRule 
+	public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();	
+	@Rule
+    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+	
+	@Autowired
+	private PersonasDAO personasDAO;
+	
+	@Test
+	public void testPrueba(){		
+		assertEquals(2,personasDAO.sumados());		
+	}
+}
